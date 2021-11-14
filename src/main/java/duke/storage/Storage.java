4d94c3694f7225837728d5766e3b69f785510ab7
e0262191 <54852720+e0262191@users.noreply.github.com>
2021-11-14 23:06:40 +0800

    Storage

--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -1,14 +1,30 @@
-package storage;
+package duke.storage;
 
+import java.io.FileNotFoundException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import duke.task.Task;
+import java.io.File;
+import java.util.Scanner;
 
 public class Storage {
-    private final Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
-    private String filePath;
 
-    public Storage(String filePath) {
-        this.filePath = "/" + filePath;
+    private static final String SAVE = ";";
+    private String Path;
+
+    public Storage(String Path, Task task) throws FileNotFoundException {
+        this.Path = Path;
+        File file = new File(this.Path);
+        if (file.isFile()) {
+            loadData(file, task);
+
+        }
+    }
+
+    private void loadData(File file, Task task) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+        String line = scan.nextLine();
+        task.getTask();
     }
 }
 
